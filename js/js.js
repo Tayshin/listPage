028d4e5053d638753b90589f1110223d86944cc3
DaiZhen <daiz1996@qq.com>
2017-02-27 23:48:57 +0800

    finished

---
+++ js/js.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    var active = document.getElementsByClassName('active')[0];
+    var btn1 = document.getElementsByClassName('header-employer')[0];
+    var btn2 = document.getElementsByClassName('header-carer')[0];
+    btn2.addEventListener('click',function(){
+        btn1.style.color = '#ffffff';
+        btn2.style.color = '#5fc0cd';
+        active.style.left = 'auto';
+        active.style.right = '0';
+    },false);
+    btn1.addEventListener('click',function(){
+        btn2.style.color = '#ffffff';
+        btn1.style.color = '#5fc0cd';
+        active.style.right = 'auto';
+        active.style.left = '0';
+    },false);
+};
